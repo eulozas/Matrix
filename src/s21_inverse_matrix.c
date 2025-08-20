cf6ab0dfeb5b32a32c7925af98c632a0f584f94b
Humfrey Jake <humfreyj@student.21-school.ru>
2025-08-20 11:56:15 +0300

    red inverse func(result for 1x1 matrix)

--- src/s21_inverse_matrix.c
+++ src/s21_inverse_matrix.c
@@ -13,22 +13,18 @@ int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
 
   if (!exit_code) {
     s21_create_matrix(A->rows, A->columns, result);
-    if (A->rows == 1 && A->columns == 1) {
-      result->matrix[0][0] = 1.0 / A->matrix[0][0];
+    double determinant;
+    s21_determinant(A, &determinant);
+    if (fabs(determinant) < EPS) {
+      exit_code = 2;
     } else {
-      double determinant;
-      s21_determinant(A, &determinant);
-      if (fabs(determinant) < EPS) {
-        exit_code = 2;
-      } else {
-        matrix_t calc_matrix;
-        s21_calc_complements(A, &calc_matrix);
-        matrix_t transp_matrix;
-        s21_transpose(&calc_matrix, &transp_matrix);
-        s21_remove_matrix(&calc_matrix);
-        s21_mul_det(&transp_matrix, determinant, result);
-        s21_remove_matrix(&transp_matrix);
-      }
+      matrix_t calc_matrix;
+      s21_calc_complements(A, &calc_matrix);
+      matrix_t transp_matrix;
+      s21_transpose(&calc_matrix, &transp_matrix);
+      s21_remove_matrix(&calc_matrix);
+      s21_mul_det(&transp_matrix, determinant, result);
+      s21_remove_matrix(&transp_matrix);
     }
   }
   return exit_code;
